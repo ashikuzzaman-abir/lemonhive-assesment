de955021a99819027142353134946bd3d9c6605e
Ashikuzzaman Abir <ashikuzzamanabir@outlook.com>
2022-09-05 01:48:03 +0600

    conferenceContent final update.jsx

--- components/ConferenceContent.jsx
+++ components/ConferenceContent.jsx
@@ -12,7 +12,7 @@ function ConferenceContent({ speakers, loading }) {
         return (
           <div
             key={i}
-            className=" flex sm:gap-5 lg:gap-10 sm:mb-5 lg:mb-6 sm:p-2 lg:p-8"
+            className=" flex sm:gap-5 lg:gap-10 sm:mb-5 lg:mb-6 sm:py-2 sm:px-6 lg:p-8"
           >
             <div className="relative sm:w-[88px] sm:h-[88px] lg:w-[140px] lg:h-[140px] rounded-lg ">
               <Image
@@ -23,12 +23,12 @@ function ConferenceContent({ speakers, loading }) {
                 objectPosition="center"
               />
             </div>
-            <div className="w-full flex flex-col justify-center gap-5">
+            <div className="w-full flex flex-col justify-center sm:gap-2 lg:gap-5">
               <div className="flex sm:flex-col lg:flex-row lg:items-center justify-between">
                 <h3 className=" sm:text-h4 lg:text-h3 font-bold text-bg-darkBlue">
                   {item.name}
                 </h3>
-                <h4 className=" sm:text-h5 lg:text-h4 font-medium text-shadowGrey">
+                <h4 className=" sm:text-h5 sm:mt-2 lg:mt-0 lg:text-h4 font-medium sm:text-[#0045FF] lg:text-shadowGrey">
                   {item.company || "Company Name"}
                 </h4>
               </div>
